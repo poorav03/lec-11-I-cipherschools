2ea8ab9fab01eb896d676be3820a25f2f66fe59a
Poorav <118240170+poorav03@users.noreply.github.com>
2023-10-17 20:22:13 +0530

    Add files via upload

---
+++ Lecture11-I.cpp
@@ -0,0 +1,12 @@
+// Lecture 11 I - Address of Elements in C++ Pt 1
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "The size of int is : " << sizeof(int) << endl;
+    cout << "The size of long int is : " << sizeof(long int) << endl;
+    cout << "The size of long long int is : " << sizeof(long long int) << endl;
+    cout << "The size of float is : " << sizeof(float) << endl;
+    cout << "The size of double is : " << sizeof(double) << endl;
+    cout << "The size of char is : " << sizeof(char) << endl;
+}
